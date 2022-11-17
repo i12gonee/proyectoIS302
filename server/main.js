0f420eea6542a293e36ba46762a1222d83fb9e04
Pacatro <paco.algar.15@gmail.com>
2022-11-17 20:54:47 +0100

    creando servidor

---
+++ server/main.js
@@ -0,0 +1,11 @@
+const express = require('express')
+
+const app = express()
+
+const port = 8000
+
+app.use(express.static('../client'))
+
+app.listen(port, () => {
+    console.log('Listening in port', port)
+})
