47aaf3886a6b12b023aba26cd2c33444ede6baa5
Pacatro <paco.algar.15@gmail.com>
2022-11-18 13:51:49 +0100

    server added

--- server/main.js
+++ server/main.js
@@ -1,11 +1,11 @@
 const express = require('express')
+const path = require('path')
 
 const app = express()
-
 const port = 8000
 
-app.use(express.static('../client'))
+app.use(express.static(path.join(__dirname, '../client')))
 
 app.listen(port, () => {
-    console.log('Listening in port', port)
+    console.log('Listening in http://localhost:'+port)
 })
