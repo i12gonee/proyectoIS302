70d8fa4787bbd3bb8946c64c3f7aa805e763943a
Pacatro <paco.algar.15@gmail.com>
2022-11-23 12:44:02 +0100

    cood_recursos_acabado

--- classes/coord_recursos.js
+++ classes/coord_recursos.js
@@ -1,5 +1,36 @@
 import Usuario from './usuario.js'
+import Curso from './curso.js'
 
 class Coord_Recursos extends Usuario{
+    #recursos_coord_ = []
 
+    constructor(id_usuario, nombre_completo, correo_electronico, contraseña){
+        super(id_usuario, nombre_completo, correo_electronico, contraseña)
+    }
+
+    añadir_recurso(recurso){
+        for(let i = 0; i<this.#recursos_coord_.length; i++){
+            if(this.#recursos_coord_.at(i) === recurso){
+                return false
+            }
+        }
+
+        this.#recursos_coord_.push(recurso)
+        return true        
+    }
+
+    eliminar_recurso(recurso){
+        for(let i = 0; i<this.#recursos_coord_.length; i++){
+        	if(this.#recursos_coord_.at(i) === recurso){
+        		this.#recursos_coord_.slice(i)
+        		return true
+        	}
+        }
+        
+        return false
+    }
+    
+    modificar_recursos(curso){
+    	curso.recursos = this.#recursos_coord_
+    }
 }
