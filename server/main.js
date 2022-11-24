93875d6e6f262a4ba2731a0db84edffa731bfa6e
Pacatro <paco.algar.15@gmail.com>
2022-11-24 22:00:46 +0100

    conexión con la bd

--- server/main.js
+++ server/main.js
@@ -8,35 +8,40 @@ const urlencodedParser = bodyParser.urlencoded({ extended: false })
 const app = express()
 const port = 8000
 
-/*connect to database (local)
+//connect to database (local)
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'i12almuf',
     password: 'basededatos_is',
     database: 'bd_is'
 })
-*/
 
 app.use('/', express.static(path.join(__dirname, '../client')))
 
-app.post('/', urlencodedParser, (req, res) => {
-    console.log(`Usuario: ${req.body.usuario}\nContraseña: ${req.body.contraseña}`)
-    res.redirect('/')
-})
-
 /*
-app.get('/bd',  (req, res) => {
+app.post('/', urlencodedParser, (req, res) => {
+    let nombre = req.body.usuario
+    let contraseña = req.body.contraseña
+
+    console.log(`Usuario: ${nombre}\nContraseña: ${contraseña}`)
     connection.connect()
 
-    connection.query('SELECT 1 + 1 AS solution', (err, rows, fields) => {
+    connection.query(`SELECT nombre_cc, contrasena_cc FROM COORD_CURSOS WHERE nombre_cc = '${nombre}'`, (err, rows, fields) => {
         if(err) throw err
-        console.log(rows[0].solution)
-        //console.log(fields)
+        console.log(rows)
+
+        if(rows[0].contrasena_cc !== contraseña){
+            return res.sendStatus(400)
+        }
+
+        res.redirect('/')
     })
 
-    connection.end()
+    //connection.end()
+
 })
 */
+
 app.listen(port, () => {
     console.log('Listening in http://localhost:'+port)
 })
