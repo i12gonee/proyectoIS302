5f7f50e856a54847d861f841cee480fc22203949
Emifox03 <koala032003@hotmail.com>
2022-11-26 11:42:55 +0100

    Update registroscript.js

---
+++ client/inicreg/registroscript.js
@@ -0,0 +1,13 @@
+const style= `
+transform: translateY(2rem) scale(1);
+`;
+
+function arrimba(){
+    document.getElementById("usu").classList.add("funcionactive");
+    document.getElementById("usu").style.cssText=style;
+}
+
+function arrimba2(){
+    document.getElementById("contra").classList.add("funcionactive2");
+    document.getElementById("contra").style.cssText=style;
+}
