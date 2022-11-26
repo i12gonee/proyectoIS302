c9f8599d4fc2fc1d335d1705e3ce611c3ab4f905
Emifox03 <koala032003@hotmail.com>
2022-11-26 12:16:03 +0100

    inputs

--- client/inicreg/registroscript.js
+++ client/inicreg/registroscript.js
@@ -3,11 +3,11 @@ transform: translateY(2rem) scale(1);
 `;
 
 function arrimba(){
-    document.getElementById("usu").classList.add("funcionactive");
-    document.getElementById("usu").style.cssText=style;
+    document.getElementById("nomb").classList.add("funcionactive");
+    document.getElementById("nomb").style.cssText=style;
 }
 
 function arrimba2(){
-    document.getElementById("contra").classList.add("funcionactive2");
-    document.getElementById("contra").style.cssText=style;
+    document.getElementById("aplld").classList.add("funcionactive2");
+    document.getElementById("aplld").style.cssText=style;
 }
