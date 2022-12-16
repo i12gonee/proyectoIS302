069b671f2e992f2b063e6dfcbf9bd3057641b788
angeladela <118201038+i12enpea@users.noreply.github.com>
2022-12-16 15:47:26 +0100

    gyarados

--- client/coordcursos/form_delete/form_delete.js
+++ client/coordcursos/form_delete/form_delete.js
@@ -2,10 +2,6 @@ const style= `
 transform: translateY(2rem) scale(1);
 `;
 
-function arrimba(){
-    document.getElementById("usu").classList.add("funcionactive");
-    document.getElementById("usu").style.cssText=style;
-}
 
 function arrimba2(){
     document.getElementById("contra").classList.add("funcionactive2");
